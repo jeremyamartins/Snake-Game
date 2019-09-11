da3b2e94674c456deade8877ba0abf2edf6970e9
Jeremy <jeremyamartins@gmail.com>
2019-09-11 00:07:12 -0400

    Gamer window added, background color is currently green

--- snake.py
+++ snake.py
@@ -1,3 +1,19 @@
+import turtle
 
+
+wn = turtle.Screen()
+wn.title("Snake Game by: Map The Coder")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+
+
+
+
+
+wn.mainloop()
 
 
